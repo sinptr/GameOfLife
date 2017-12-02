d01d9b458069ca23416c8ed8a5cf9c8e2b122a46
sp <zoneofatombomb@gmail.com>
2017-12-02 18:32:19 +0300

    new function of next generation

--- source/main.py
+++ source/main.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python
 
+import numpy as np
+
+def life_step_1(X):
+    """Game of life step using generator expressions"""
+    nbrs_count = sum(np.roll(np.roll(X, i, 0), j, 1)
+                     for i in (-1, 0, 1) for j in (-1, 0, 1)
+                     if (i != 0 or j != 0))
+    return (nbrs_count == 3) | (np.uint64(X) & (nbrs_count == 2))
+
+
 import itertools
 import pygame
 import time
@@ -7,8 +17,7 @@ import sys
 from pygame.locals import *
 
 
-size = 20
-TILESIZE = int(600/(size))
+
 
 
 def exit_game():
@@ -71,7 +80,8 @@ def correct_cons(size, cons):
     # return filter(lambda x: is_correct_con(size, x), cons)
     cor_cons = list()
     for con in cons:
-        cor_cons.append(correct_con(size, con))
+        if correct_con(size, con) not in cor_cons:
+            cor_cons.append(correct_con(size, con))
     # return [x for x in cons if is_correct_con(size, x)]
     return cor_cons
 
@@ -110,16 +120,39 @@ def print_board(surf, board):
             rect = pygame.Rect(x * TILESIZE, y * TILESIZE, TILESIZE, TILESIZE)
             pygame.draw.rect(surf, color, rect, 0)
             pygame.draw.rect(surf, (0, 0, 0), rect, 1)
-            #draw_grid(surf, l)
+
+
+def get_active_cell(mouse_pos, size_row, size_col):
+    """
+    :param      mouse_pos:  mouse position on widow screen
+
+    :return:    cell:   cell... just cell
+    """
+    for row in range(size_row):
+        for column in range(size_col):
+            if (column * TILESIZE) <= mouse_pos[0] <= (column * TILESIZE) + TILESIZE:
+                if (row * TILESIZE) <= mouse_pos[1] <= (row * TILESIZE) + TILESIZE:
+                    cell = (row, column)
+                    return cell
+
+
+def draw_cell(surf, pos, color=(0, 0, 0)):
+    rect = pygame.Rect(pos[1] * TILESIZE, pos[0] * TILESIZE, TILESIZE, TILESIZE)
+    pygame.draw.rect(surf, color, rect, 0)
+    pygame.draw.rect(surf, (0, 0, 0), rect, 1)
 
 
 def main():
+    global STOP
+    boards = list()
     pygame.init()
-    board = [(1, 2), (2, 3), (3, 1), (3, 2), (3, 3), (4, 3), (1, 1), (2, 2), (5, 1)]
-    surf = pygame.display.set_mode((size * TILESIZE, size * TILESIZE))
+    board = np.zeros((size, size))
+    surf = pygame.display.set_mode((size * TILESIZE + 200, size * TILESIZE))
+
     surf.fill((255, 255, 255))
     draw_grid(surf, size)
-    print_board(surf, get_board(size, board))
+    print_board(surf, board)
+    draw_cell(surf, (0, size + 1), (255, 0, 0))
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -128,12 +161,29 @@ def main():
                     # board = correct_cons(size, new_step(board))
                     # print_board(surf, get_board(size, board))
                     # time.sleep(0.5)
+                    mouse_pos = pygame.mouse.get_pos()
+                    act_cell = get_active_cell(mouse_pos, size, size + 2)
+                    print(act_cell)
+                    if act_cell == (0, size + 1):
+                        STOP = not STOP
+                    elif act_cell:
+                        draw_cell(surf, act_cell, (0, 255, 0))
+                        board[act_cell[1], act_cell[0]] = 1
                     pygame.display.update()
-        board = correct_cons(size, new_step(board))
-        print_board(surf, get_board(size, board))
-        time.sleep(0.2)
+        if not STOP:
+            board = life_step_1(board)
+            if any((board == x).all() for x in boards):
+                STOP = not STOP
+            else:
+                boards.append(board)
+
+            print_board(surf, board)
+            time.sleep(0.1)
         pygame.display.update()
 
 
 if __name__ == '__main__':
+    size = 30
+    TILESIZE = int(600 / (size))
+    STOP = 1
     main()
